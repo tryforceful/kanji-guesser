3fcea94bdf786582173d0444bf23babecb9638c3
tryforceful <tryforceful@gmail.com>
2020-01-22 21:16:40 -0500

    Refactored KanjiGuesser as React class TextPlaceholder has customizable color Adding some source data from KanjiTester

--- src/components/TextPlaceholder.tsx
+++ src/components/TextPlaceholder.tsx
@@ -1,13 +1,24 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-export default styled.div`
-  position: relative;
-  bottom: -5px;
-  background-color: var(--ion-color-primary-tint);
-  height: 40px;
-  width: 40px;
-  display: inline-block;
-  border-radius: 6px;
-  opacity: 0.5;
-  vertical-align: baseline;
-`;
+interface Props {
+  variant?: string
+}
+
+// prettier-ignore
+const TextPlaceholder = styled.div`
+    position: relative;
+    bottom: -5px;
+    background-color: var(--ion-color-${(props: Props) => props.variant}-tint);
+    height: 40px;
+    width: 40px;
+    display: inline-block;
+    border-radius: 6px;
+    opacity: 0.5;
+    vertical-align: baseline;
+  `
+
+TextPlaceholder.defaultProps = {
+  variant: "primary"
+}
+
+export default TextPlaceholder
